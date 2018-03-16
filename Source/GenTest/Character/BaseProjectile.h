6a43686fc4a31ae317f8e6014bbb21638645538d
Alex Hesketh <alex@alexhesketh.co.uk>
2018-03-16 03:32:22 +0000

    Weapon + Projectile Overhaul

--- Source/GenTest/Character/BaseProjectile.h
+++ Source/GenTest/Character/BaseProjectile.h
@@ -2,6 +2,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "ProjectileDamageComponent.h"
 #include "BaseProjectile.generated.h"
 
 UCLASS()
@@ -10,19 +11,14 @@ class GENTEST_API ABaseProjectile : public AActor
 	GENERATED_BODY()
 
 private:
-	AActor* ActorWhichFired;
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
+	UProjectileDamageComponent* ProjectileDamage;
 
 	UPROPERTY(Category = Mesh, VisibleDefaultsOnly, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
 	class UStaticMeshComponent* ProjectileMeshComponent;
 
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = Movement, meta = (AllowPrivateAccess = "true"))
 	class UProjectileMovementComponent* ProjectileMovement;
-
-	UPROPERTY(Category = "Projectile", EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
-	int ProjectileDamage;
-
-	UPROPERTY(Category = "Projectile", EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
-	bool bProjectilePiercesActors;
 
 	void InitialiseStaticMesh();
 	void InitialiseProjectileMove();
@@ -35,9 +31,6 @@ public:
 
 	UFUNCTION()
 	void OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult &SweepResult);
-
-	UFUNCTION()
-	void SetFiringActor(AActor* shooter);
 
 	virtual void Tick(float DeltaSeconds);
 };
