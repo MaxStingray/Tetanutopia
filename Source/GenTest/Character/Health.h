870bb57882f78a317d4a4f742a598b822344764b
Alex Hesketh <alex@alexhesketh.co.uk>
2018-03-16 14:45:00 +0000

    Added Area Damage Base.

--- Source/GenTest/Character/Health.h
+++ Source/GenTest/Character/Health.h
@@ -2,7 +2,7 @@
 #include "CoreMinimal.h"
 #include "Health.generated.h"
 
-UINTERFACE(MinimalAPI)
+UINTERFACE(MinimalAPI, meta = (CannotImplementInterfaceInBlueprint))
 class UHealth : public UInterface
 {
 	GENERATED_BODY()
@@ -15,8 +15,8 @@ private:
 	//UFUNCTION(BlueprintCallable)
 	virtual void OnDeath();
 public:
-	//UFUNCTION(BlueprintCallable)
-	virtual void TakeDamage(int value) = 0;
-	//UFUNCTION(BlueprintCallable)
-	virtual void Heal(int value) = 0;
+	UFUNCTION(BlueprintCallable)
+	virtual void TakeDamage(int value) {};
+	UFUNCTION(BlueprintCallable)
+	virtual void Heal(int value) {};
 };
